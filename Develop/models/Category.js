0dc92673029b58713bcb2e694346505af951d3a1
Alpha Bravo <bautista.albert@gmail.com>
2022-09-11 21:18:14 -0500

    Update schema.sql, all javascript files in models, routes and server.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,12 +1,22 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
